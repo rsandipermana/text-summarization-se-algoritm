c01fc7af1c2d3f4a4be3fd291279506bb999acc3
Sandi Permana <rsandipermana@github.com>
2023-01-02 20:39:45 +0700

    display title in article admin

--- app/app/admin.py
+++ app/app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Article, Summary, Evaluation
-
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Summary)
 admin.site.register(Evaluation)
